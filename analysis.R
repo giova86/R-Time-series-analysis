17cae310f2fd97c6dc3d560a9769e1e96a5db89c
Giovanni <giovanni.bocchi@gmail.com>
2019-03-25 00:59:42 +0100

    cluster update

--- analysis.R
+++ analysis.R
@@ -53,3 +53,55 @@ library(ggplot2)
 autoplot(f)
 accuracy(f)
 
+##############################################
+# Time series clustering
+##############################################
+
+# Data
+# http://kdd.ics.uci.edu/databases/synthetic_control/synthetic_control.html
+
+data <- read.csv(file.choose(), header = FALSE, sep = '')
+str(data)
+plot(data[,60], type = 'l')
+j <- c(5, 105, 205, 305, 405, 305)
+sample <- t(data[j,])
+plot.ts(sample,
+        main = 'Time-series plot',
+        col = 'blue',
+        type = 'b')
+
+# Data preparation
+n <- 10
+s <- sample(1:100, 10)
+i <- c(s, 100+s, 200+s, 300+s, 400+s, 500+s)
+d <- data[i, ]
+
+pattern <- c(rep('Normal', n),
+             rep('Cyclic', n),
+             rep('Increasing Trend', n),
+             rep('Decreasing Trend', n),
+             rep('Upward Shift', n),
+             rep('Downward Shift', n)
+             )
+
+# Calculate distances
+library(dtw)
+distance <- dist(d, method = 'DTW')
+
+# Hierarchical Clustering
+hc <- hclust(distance, method = 'average')
+plot(hc,
+     labels = pattern,
+     cex = 0.5,
+     hang = -1,
+     col = 'blue'
+)
+rect.hclust(hc, k=4)
+rect.hclust(hc, k=2)
+
+
+ 
+
+
+
+
